42f1bc8281740d3818fba591bc7e00b3746fb55f
oluwatobi balogun <tobibags19@gmail.com>
2019-06-11 03:49:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, obj)
+  var clone = Object.assign({}, obj)
   
-  delete newObj.
+  delete clone
 }
